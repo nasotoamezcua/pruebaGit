40c598c6ac4a37ea0d38a402cf35282399a88e1c
Nestor Soto <nasotoamezcua@gmail.com>
2017-08-30 16:36:27 -0500

    Operaciones Clase 1

---
+++ practica1-interfaces/src/main/java/org/certificatic/practica1/interfaces/operations/api/ISimpleCalculator.java
@@ -0,0 +1,10 @@
+package org.certificatic.practica1.interfaces.operations.api;
+
+public interface ISimpleCalculator 
+	extends ICalculator<ISimpleCalculator>,
+		IAdd<ISimpleCalculator>,
+		ISubtract<ISimpleCalculator>,
+		IDivide<ISimpleCalculator>,
+		IMultiply<ISimpleCalculator>{
+
+}
--- practica1-interfaces/src/test/java/org/certificatic/practica1/interfaces/operations/test/KidsCalculatorTest.java
+++ practica1-interfaces/src/test/java/org/certificatic/practica1/interfaces/operations/test/KidsCalculatorTest.java
@@ -2,6 +2,11 @@ package org.certificatic.practica1.interfaces.operations.test;
 
 import java.text.DecimalFormat;
 
+import org.certificatic.practica1.interfaces.operations.api.IKidsCalculator;
+import org.certificatic.practica1.interfaces.operations.api.ISimpleCalculator;
+import org.certificatic.practica1.interfaces.operations.api.impl.KidsCalculator;
+import org.certificatic.practica1.interfaces.operations.api.impl.SimpleCalculator;
+import org.junit.Assert;
 import org.junit.Test;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +21,16 @@ public class KidsCalculatorTest {
 		log.info("kidsCalculatorTest -------------------");
 		
 		// Implementar
+		
+		ISimpleCalculator simple = new SimpleCalculator();
+		IKidsCalculator kids = new KidsCalculator();
+		
+		final double expectedValue  = 16D;
+		final double result  = kids.set(5).add(5).add(5).add(5).substract(4).result();
+		
+		Assert.assertEquals(expectedValue, result, 0.0001);
+		
+		
 	}
 	
 }
