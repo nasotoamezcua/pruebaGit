548b5cae301680499d9a17457d1a7da082e075ed
Nestor Soto <nasotoamezcua@gmail.com>
2017-10-15 22:03:08 -0500

    labCurso CLase 6 OK

---
+++ 3-spring-data-jdbc/src/main/resources/db/data.sql
@@ -0,0 +1,5 @@
+insert into CUSTOMER_TBL values(1, 'Ivan Venor', 'García');
+insert into USER_TBL values(1, 1, 'xvanhalenx', '123123');
+
+insert into ACCOUNT_TBL values(1, 1, '00112233445566', '2016-02-28', 125590.55);
+insert into ACCOUNT_TBL values(2, 1, '00112233445577', '2016-02-20', 150000.00);
