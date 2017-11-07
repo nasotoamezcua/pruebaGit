8a68684cb9d9c3615627746026f8382ed774d058
NESTOR ALEJANDRO SOTO AMEZCUA <VNGAE40@telcel.net>
2017-11-07 11:57:56 -0600

    labNestor Clase 9:5-spring-web-mvc-hello-world --> Practica 28,29 y 30 OK

--- 5-spring-web/5-spring-web-mvc-hello-world/src/main/java/org/certificatic/spring/mvc/practica30/parte2/forms/ContactForm.java
+++ 5-spring-web/5-spring-web-mvc-hello-world/src/main/java/org/certificatic/spring/mvc/practica30/parte2/forms/ContactForm.java
@@ -2,13 +2,18 @@ package org.certificatic.spring.mvc.practica30.parte2.forms;
 
 import java.util.List;
 
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class ContactForm {
+	
+	@Size(min = 6, max =20, message = "name: Debe contener minimo 6 caracteres")
 	private String name;
 	private String email;
 	private String gender;
+	@Size(min = 5, max =10, message = "password: Debe contener entre 5 y 10 caracteres")
 	private String password;
 	private String confirmPassword;
 	private List<String> courses;
